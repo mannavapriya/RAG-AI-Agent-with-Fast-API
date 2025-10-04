bc7c54dc9a5ff295dc31f767a5cdcf1dd78fb894
manasa <mmannavapriya@gmail.com>
2025-10-04 07:47:41 -0500

    updated prompt

--- rag_api.py
+++ rag_api.py
@@ -47,16 +47,13 @@ async def get_conversational_chain():
         contextualize_q_prompt = ChatPromptTemplate.from_messages(
             [
                 ("system",
-                "You are Nomi, a travel assistant. "
-                "You only answer questions based strictly on the provided knowledge base context. "
-                "If the answer is not found in the context, reply exactly with: 'I don’t know.' "
-                "Do not make up information or provide general travel tips. "
-                "Here is the knowledge base context:\n\n{context}"),
+                 "You are Nomi, a travel assistant. "
+                 "You only answer questions in the knowledge base {context} "
+                 "If the question is outside this knowledge base, reply: 'I'm sorry, I don't know.'\n\n{context}"),
                 MessagesPlaceholder("chat_history"),
                 ("human", "{input}")
             ]
         )
-
         history_aware_retriever = create_history_aware_retriever(llm, retriever, contextualize_q_prompt)
 
         qa_prompt = ChatPromptTemplate.from_messages(
